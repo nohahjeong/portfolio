2bc2b83145483bccc9c52c9afe1a8c1f100b8d57
Noh Ah Jeong <noh_ah@hotmail.com>
2021-04-01 15:58:01 -0300

    bio

--- src/components/Bio/styled.js
+++ src/components/Bio/styled.js
@@ -6,30 +6,25 @@ export const BioContainer = styled.div`
     display: flex;
     justify-content: space-around;
     align-items: center;
-    color: #ffffff;
-
     background-color: #063447;
+    color: #ffffff;
 `
 
 export const Row = styled.div`
     display: flex;
     align-items: center;
-    gap: 10vw;
+    gap: 100px;
 
     .photo {
-        width: 30vw;
-        max-width: 305px;
-        height: 30vw;
-        max-height: 305px;
+        width: 305px;
+        height: 305px;
     }
 `
 
 export const BioText = styled.div`
-    width:40vw;
-    max-width: 500px;
-    height:30vw;
-    max-height: 305px;
+    width: 370px;
+    height: 305px;
     display: flex;
     flex-direction: column;
-    gap: .4rem;
+    gap: 20px;
 `
